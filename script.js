67c63f5e444b12707cec8f00019bbf474e53b049
blueyghes <gegeworks16@gmail.com>
2026-06-06 03:00:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -205,7 +205,7 @@ const darkButton = document.createElement("button");
 
 darkButton.innerHTML = "🌙";
 
-darkButton.classList.add("back-to-top");
+darkButton.classList.add("dark-toogle");
 
 darkButton.style.bottom = "95px";
 
